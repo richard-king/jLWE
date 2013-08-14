920f3165962602f787e9c312dca9c401c67984a3
Richard King <richard@richardking.me>
2013-08-14 01:51:23 +0100

    Added Vector w/ basic functions

---
+++ src/math/Vector.java
@@ -0,0 +1,41 @@
+package math;
+
+import java.util.Random;
+
+public class Vector
+{
+	private int[] v;
+	private int m, r;
+	
+	public Vector(int m, int r)
+	{
+		this.m = m;
+		this.v = new int[m];
+		this.r = r;
+		init();
+	}
+	
+	public Vector(int[] v, int r)
+	{
+		this.v = v;
+		this.r = r;
+		this.m = v.length;
+	}
+	
+	private void init()
+	{
+		Random random = new Random();
+		for(int i = 0; i < this.m; ++i)
+			this.v[i] = random.nextInt(this.r) - this.r;
+	}
+	
+	public int get(int i)
+	{
+		return this.v[i];
+	}
+	
+	public void set(int i, int n)
+	{
+		this.v[i] = n;
+	}
+}
